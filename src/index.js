c24df1544f9d6c541743154518b9ec42e247fb11
Sarfraz Akhtar <sarfraz.akhtar@discorp.com>
2024-04-24 20:13:47 +0500

    adding eslint-plugin-vuetify-v1.1 locally

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const requireindex = require('requireindex')
 const args = process.argv;
-const pluginV11 = require('eslint-plugin-vuetify-v1.1');
+const pluginV11 = require(path.join(__dirname, '../eslint-plugin-vuetify-v1.1/lib'));
 const plugin = args.includes('eslint-vuetify2') ? pluginV11 : ({
   configs: requireindex(path.join(__dirname, './configs')),
   rules: requireindex(path.join(__dirname, './rules')),
